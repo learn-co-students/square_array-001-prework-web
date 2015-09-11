5465d00cb3ea027f64d9c7bb8c4d734a4865c54f
moziff <matteoziff@gmail.com>
2015-09-11 12:05:53 -0400

    done with second array

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,8 @@
 def square_array(array)
-  # your code here
+  array_new =Array.new
+  array.each do |numbers|
+      numbers=numbers**2
+      array_new.push(numbers)
+      end
+  return array_new
 end
