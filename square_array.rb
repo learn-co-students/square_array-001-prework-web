5f8df26acca4249361d756a7e77d2053eebbbc02
leighsn <leighscherrer@gmail.com>
2015-11-06 06:35:31 -0500

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,14 @@
+
+
+
 def square_array(array)
+  squared_array = []
+  count = 0
   # your code here
+  array.each do |item|
+    squared_array[count] = item **2
+    count += 1
+  end
+  return squared_array
 end
+
