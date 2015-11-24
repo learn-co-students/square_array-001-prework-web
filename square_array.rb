767fd8c748980901ba1dbbb33c8937c386594318
Amger <albertgertskis@gmail.com>
2015-11-23 22:15:53 -0500

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,10 @@
 def square_array(array)
   # your code here
+  squared_array = []
+
+  #for each element in #array, shovel the square of that element to #squared_array
+  array.each { |element| squared_array << element ** 2}
+
+  #return the squared values of #array
+  squared_array
 end
