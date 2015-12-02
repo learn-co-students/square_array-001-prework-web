14c1b4933b67886ea245238b425ee1d52d382cfa
tintinmar <wangmartinxy@gmail.com>
2015-12-01 19:22:44 -0500

    solved

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,10 @@
 def square_array(array)
   # your code here
+ new_array = []
+  # your code here
+  array.each do |item|
+    item = item ** 2
+    new_array << item
+  end
+  array = new_array
 end
