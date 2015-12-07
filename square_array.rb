c453a805b8601fa2fa36812f6bed196a69cd309b
mindyyuan <mindyuan@gmail.com>
2015-12-06 20:56:31 -0500

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,11 @@
 def square_array(array)
-  # your code here
+n=0
+square_array=[]
+    while n < array.length
+    array.each do |element|
+        square_array.push(element**2)
+        n+=1
+    end
+    return square_array
+    end
 end
