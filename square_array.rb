456d21aa9fa8886ca462fd792e89d097f8e1d2dc
Alex Montolio <alex.montolio@gmail.com>
2015-12-18 01:31:35 -0500

    Added .collect method in comments

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,10 @@
 def square_array(array)
-  # your code here
+	#Alternative with array.collect
+	#array.collect {|x| x ** 2}
+	new_array = Array.new
+	array.each do |element|
+		new_array.push(element ** 2)
+	end
+	
+	return new_array
 end
