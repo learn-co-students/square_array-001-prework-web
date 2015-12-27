fed0f7dbfa0d35cbaf626fe7784fa28dd4556003
evzaro <evzaro@gmail.com>
2015-12-27 04:19:45 -0500

    Square Array & Collect Lab

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,11 @@
 def square_array(array)
-  # your code here
+  new_arr = []
+  array.each do |num| 
+    new_arr.push(num**2)
+  end
+    new_arr
+end
+
+def collect_array(array)
+  array.collect!{|x| x**2}
 end
