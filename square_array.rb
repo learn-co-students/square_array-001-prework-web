7d2a26496a95df92e0febb612b4899b5f53956c1
LGOLIK <golikster@gmail.com>
2015-12-29 08:59:41 -0500

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,10 @@
 def square_array(array)
   # your code here
+  sq_array = []
+  array.each { |e| sq_array << e**2 }
+  sq_array
+end
+
+def complex_square_array(array)
+  sq_array = array.collect { |e| e**2 }
 end
