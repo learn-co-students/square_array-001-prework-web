ff37f96fc6ebec4956f4f116e3f792498c25568a
hdwatts <howard@hdwatts.com>
2016-01-06 17:25:57 -0500

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,12 @@
 def square_array(array)
-  # your code here
+  squared = []
+
+  array.each do |num|
+    squared.push(num * num)
+  end
+
+  squared
+
+  #running with collect
+  #array.collect { |x| x * x }
 end
