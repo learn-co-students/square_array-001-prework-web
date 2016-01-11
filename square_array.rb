b0c57d9ad771647a7069391cb68caae3379e6ac0
WilliamMena <williammena1991@gmail.com>
2016-01-10 21:21:36 -0500

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,10 @@
 def square_array(array)
   # your code here
+  new_array = []
+array.each {|squared| new_array << (squared**2)}
+  new_array
+  ##thanks to TyJuanABrooks again
+
 end
+#numbers = [1,2,3]
+#square_array(numbers)
