8fed2e26c483a19b889888e2b061eafe190b1102
jjseymour <jjseymour@gmail.com>
2016-01-23 15:08:41 -0500

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,16 @@
 def square_array(array)
   # your code here
+  squared_array = []
+  #index_num = 0
+  array.each do |number|
+    squared_array << number ** 2
+    #array.shift
+    #array << number ** 2
+    #puts "Array is #{array} here"
+    #puts "Number at this point is #{number}"
+  end
+  return squared_array
 end
+
+#test_array = [1, 2, 3]
+#square_array(test_array)
