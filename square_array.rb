901e676a0c1445a114fc6827e0520867429039f7
Wen Tran <wentran@Wen-Trans-MacBook-Pro.local>
2016-02-15 10:55:26 -0500

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,11 @@
-def square_array(array)
-  # your code here
+ def square_array(old_array)
+     new_array = []
+   old_array.each do |i| 
+       new_array.push(i * i)
+       
+  end
+    new_array
 end
+
+
+square_array [1,2,3]
