385aa9fe65777865a4685ea4fab5726063ef8be1
JennyShalai <jenny.shalai@gmail.com>
2016-02-23 10:50:21 -0500

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,26 @@
+=begin
+def square_array(array)
+  i = 0
+  j = array.size
+
+  while i < j
+    array[i] = array[i] * array[i]
+    i += 1
+  end
+  array
+end
+=end
+
+
+
+
+
 def square_array(array)
   # your code here
+  array_new = []
+  array.each do |elem|
+    elem = elem * elem
+    array_new.push(elem)
+  end
+  array_new
 end
