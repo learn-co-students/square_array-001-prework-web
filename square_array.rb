00ffc38a4a0074d0ec71b9b103331b80b52a2a5c
Sebastian <shimomichi63@gmail.com>
2016-03-04 05:56:45 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,9 @@
 def square_array(foo)
   square_array = Array.new
+  return_array = Array.new
   foo.each do |bar|
     square_array << bar**2
   end
-  square_array
+  return_array = square_array.collect
+  return_array
 end
