af7b4f0dfdcd66e2e4a3c3595a56672f47d8fadf
lgyure <lukegyure@gmail.com>
2016-03-18 17:51:44 -0400

    write square_array method

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,5 @@
-def square_array(array)
-  #array = [number] (needed?)
-  array.each do |number|
-    number ** 2 #syntax for squaring?
-  end
-  #return new array with modified elements?
+def square_array(numbers)
+  array = []
+  numbers.each {|x| array << x ** 2}
+  array
 end
