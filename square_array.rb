a6923c26097e871720030d8f7be93379adda3369
shalin1 <shalin.scupham@gmail.com>
2016-03-20 17:50:41 -0400

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,13 +1,36 @@
+require 'pry'
+
 def square_array(array)
- sq_array  = []
+ results_array  = []
  array.each do |num| 
- 	sq_array << num**2
+ 	results_array << num**2
   end
-  sq_array 
+  results_array 
 end
 
-def square_array_w_collect(array)
+## more abstract
+def square_array_w_collect(array2)
 	array.collect do |num|
 		num ** 2
 	end
 end
+
+## less abstract
+def square_array_w_while(array)
+	results_array2 = []
+
+	while array.length > 0
+		results_array2 << array[0]**2
+		array.shift
+	end
+	results_array2
+end
+
+
+
+binding.pry
+square_array([1,2,3])
+square_array_w_collect([1,2,3])
+square_array_w_while([1,2,3])
+
+
