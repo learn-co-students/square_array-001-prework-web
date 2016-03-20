ef0ca77fd98ea8d032336b625461880c73894b2e
Sebastian <shimomichi63@gmail.com>
2016-03-20 22:50:49 +0000

    nitrous-backup at Sun Mar 20 22:50:49 UTC 2016

--- square_array.rb
+++ square_array.rb
@@ -1,9 +1,5 @@
 def square_array(foo)
   square_array = Array.new
-  return_array = Array.new
-  foo.each do |bar|
-    square_array << bar**2
-  end
-  return_array = square_array.collect
-  return_array
+  foo.each { |int| square_array << int ** 2}
+  square_array
 end
