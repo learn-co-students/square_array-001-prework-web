44862f45962e23d6be52fa1ccfcfe556c3ed9d1c
lalley89 <meganlalley@gmail.com>
2016-03-31 20:08:58 -0400

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,27 @@
+array = [1, 2, 3]
+
 def square_array(array)
-  # your code here
+  output = []
+
+  array.each do |integer|
+    output << integer**2
+    #Shovel integer**2 into our output array 
+  end
+
+  return output  
 end
+
+
+
+
+#new_sq_array = square_array(array)
+
+#Math.sqrt(16) for square root
+#** exponent operator
+
+
+
+# arr = [1, 2, 3, 4, 5]
+# arr.each { |a| print a -= 10, " " }
+# # prints: -9 -8 -7 -6 -5
+# #=> [1, 2, 3, 4, 5]
