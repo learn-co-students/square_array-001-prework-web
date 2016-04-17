2eb91c6b095fbc29f04807fd42cbc69c4cde5bde
iamaimeemo <aimeemosseri@gmail.com>
2016-04-17 16:38:07 -0400

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,11 @@
+
+new_numbers = [9,10,16,25]
+
 def square_array(array)
-  # your code here
+numbers = [1,2,3]
+array.each do |int|
+ puts (int**2)
 end
+end
+
+#when i used return instead of puts, it only gave me the return of the first number, not the whole array. Why?? 
