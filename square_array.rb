fd46eccd1984d229a4cc4f40425db9eaca733914
mitchwd <mitch@mitchwd.com>
2016-04-25 14:30:42 +0930

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,11 @@
 def square_array(array)
-  new_array = []
-  array.each do |element|
-    new_array << element ** 2
-  end
-  new_array
+  # Method 1 (EACH)
+  # new_array = []
+  # array.each do |element|
+  #    new_array << element ** 2
+  # end
+  # new_array
+
+  # Method 2 (Collect)
+  array.collect! { |element| element ** 2 }
 end
