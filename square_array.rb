4a408c485628673ef1c634a9eecbe90b821dafa0
Versallly <universallly@gmail.com>
2016-04-30 03:41:56 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,14 @@
 def square_array(array)
-  # your code here
+  a = [1, 2, 3]
+  i = 0
+  array.each do |sqr|
+
+   a[i] = sqr ** 2
+    i += 1
+  end
+return a
+#  array.each_index do |i|
+   # array[i] = array[i] ** 2
+  #end
+
 end
