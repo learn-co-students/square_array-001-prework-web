96788d181e6e218f156eee42ea80d051a391c95e
armandylon <dire26463@gmail.com>
2016-05-04 19:07:38 +0000

    nitrous-backup at Wed May  4 19:07:38 UTC 2016

--- square_array.rb
+++ square_array.rb
@@ -5,19 +5,8 @@ def square_array(numbers)
    new
 end
 
-=begin
-def square_array(array)
-  # your code here
-  new_ary = []
-  array.each do |i|
-    new_ary << i*i
-  end
-  new_ary
-end
 
-=begin
 def square_array_collect(array)
   # your code here
   array.collect{|i| i*i}
 end
-=end
