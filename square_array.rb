f901d30ceaa08966a564e709fac8600a3c2a932e
skydiverb <bryanbuitrago86@gmail.com>
2016-05-14 21:48:18 -0400

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,31 @@
+
+## ======= array.each{} =========== ##
+def square_array(array)
+
+  array_new = []
+
+  array.each {|i| array_new.push(i * i)}
+
+  return array_new
+
+end
+square_array([1,2,3])
+
+## ====== array.each do|| ========= ##
+
+
+=begin
+
 def square_array(array)
   # your code here
+
+  array_new = []
+
+  array.each do |i| array_new.push(i * i)
+
+  end
+  return array_new
+
 end
+square_array([1,2,3])
+=end
