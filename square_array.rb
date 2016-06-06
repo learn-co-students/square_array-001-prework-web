5e58c21eeaf77720ee69a58407a77c97dd1958ed
SharkBroPhD <troychryssos@gmail.com>
2016-06-06 16:57:05 -0400

    Updated method square_array with code that squared each number inside a given array, transferred those to a new array, and returned those values.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,16 @@
+#dogs = [1,2,3]
+
 def square_array(array)
-  # your code here
+  new_array =[]
+
+  array.each do |x|
+    square=x**2
+    x=square
+    new_array<<x
+  end
+
+  #puts new_array
+  return new_array
 end
+
+#puts square_array(dogs)
