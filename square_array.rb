ff00b715ed1fc4aaf880f24f1120363239ee831b
Annabelf11 <fergusonannabel@gmail.com>
2016-06-08 20:23:27 +0200

    Square Array Passed

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,10 @@
-def square_array(array)
-  # your code here
+def square_array(numbers)
+
+  squared_array = []
+  numbers.each do |item|
+    squared_array << item ** 2
+  end
+  squared_array
 end
+  
+  
