9a400e81196473276b4f17826f72f229b5c180ca
Christy Loo <hellochristyloo@gmail.com>
2016-06-21 02:34:32 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,14 @@
-def square_array(array)
-  # your code here
+#def square_array(array=[1, 2, 3])
+#  x = 0
+#  array.each do
+#    Math.sqrt(x)
+#  end
+#end
+
+def square_array(numbers)
+  new_arr = []
+    numbers.each do |number|
+    new_arr << (number**2)
+    end
+    return new_arr
 end
