279a7c7a384147e734c6cf411e3c8210dd622663
smcarrera <stephanie.carrera@gmail.com>
2016-07-13 12:05:11 -0400

    square array with .each, not collect

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,15 @@
 def square_array(array)
-  # your code here
+  new_array = []
+  array.each do |x|
+    new_array << (x**2)
+  end
+  new_array
 end
+
+
+=begin
+def square_array(array)
+  new_array = []
+  new_array = array.collect{ |i| i**2 }
+end
+=end
