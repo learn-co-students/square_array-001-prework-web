7cdcbf8b81d4702ef08f501b18a4667166271518
OliviaGunton <olivia.gunton@gmail.com>
2016-07-15 11:18:51 -0400

    Iterate through array using 'each' and return new array of squared numbers

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,12 @@
+#squares each element in an array of numbers and returns a new array
+#of these squared numbers
 def square_array(array)
-  # your code here
+
+  squared = []
+  array.each do |num|
+    squared.push(num * num)
+  end
+
+  squared
+  
 end
