3d2eae69f353640816736d698191557bfcb3d563
Steven Tang <5teven7ang@gmail.com>
2016-07-27 17:53:03 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,11 @@
+require 'pry'
+
 def square_array(array)
   # your code here
+  counter = 0
+  squared_array = array
+  squared_array.each do |index|
+    squared_array[counter] = index*index
+    counter += 1
+  end
 end
