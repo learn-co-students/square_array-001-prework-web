137abef235b4eb4e6241f6327edf199651c0b24d
Michael Chang <yukuochang@gmail.com>
2016-08-01 20:53:39 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,14 @@
 def square_array(array)
   # your code here
+  array.each do
+    |basevalue|
+    array[basevalue * 2]
+  end
+
+  squareArray = []
+  for i in 0...array.size
+    squareArray.push(array[i]**2)
+  end
+
+  return squareArray
 end
